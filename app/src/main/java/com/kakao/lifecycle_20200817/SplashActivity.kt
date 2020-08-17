837916c00979318656d6a4171c7d3e00108e6686
Jinkyeong96 <69335959+Jinkyeong96@users.noreply.github.com>
2020-08-17 21:36:38 +0900

    Splash 2.5초 메인화면이동

--- app/src/main/java/com/kakao/lifecycle_20200817/SplashActivity.kt
+++ app/src/main/java/com/kakao/lifecycle_20200817/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.kakao.lifecycle_20200817
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,18 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        2.5초 로딩화면
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+//            일정시간이 지난후 실행
+//        Splash 화면 => 메인화면
+            val myIntent = Intent(mContext, MainActivity::class.java)
+            startActivity(myIntent)
+
+//        메인화면으로 이동
+            finish()
+        }, 2500)
 
     }
 }
